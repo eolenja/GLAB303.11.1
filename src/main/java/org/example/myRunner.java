1e2adb1512cb912cb1f2fa6b7e182dc2142f4d66
Elan Olenja <elan.olenja@Elans-Air.localdomain>
2024-09-17 12:18:36 -0400

    first commit

---
+++ src/main/java/org/example/myRunner.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class myRunner {
+    public static void main(String[] args) {
+
+        DemoClass dObj = new DemoClass();
+        dObj.genericsMethod(25);
+        dObj.genericsMethod("Per Scholas");
+        dObj.genericsMethod(2563.5);
+        dObj.genericsMethod('H');
+    }
+}
